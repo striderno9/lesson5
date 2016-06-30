167ca6280c5b75152cc03630da3b068ccf1d0268
Jason Almenas <i.striderno9@icloud.com>
2016-06-30 13:57:16 -0400

    changed files for practice

---
+++ style.js
@@ -0,0 +1,5 @@
+function addNums() {
+	num1 = document.getElementById('num1').value;
+	num2 = document.getElementById('num2').value;
+	document.getElementById('result').innerHTML = (parseInt(num1) + parseInt(num2));
+}
